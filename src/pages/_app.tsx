7ed3dc559d6da466d680cda90066b5e3db138010
William Wang <swang45@uw.edu>
2022-08-02 10:45:59 -0400

    Recovered Modal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,17 @@ import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import Layout from 'components/common/layout';
 import { SessionProvider } from 'next-auth/react';
+import { ContextProvider } from 'components/common/context/ContextProvider';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
-    <SessionProvider session={session}>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </SessionProvider>
+    
+    <ContextProvider>
+        <SessionProvider session={session}>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </SessionProvider>
+    </ContextProvider>
 );
 
 export default App;
