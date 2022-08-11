5e4834d53699bf72d8107609fa6b693c896c1cdf
William Wang <swang45@uw.edu>
2022-08-11 09:38:54 -0700

    import updated

--- src/components/common/wallet-adapter/wallet-list-item.tsx
+++ src/components/common/wallet-adapter/wallet-list-item.tsx
@@ -2,7 +2,7 @@ import { WalletReadyState } from '@solana/wallet-adapter-base';
 import type { Wallet } from '@solana/wallet-adapter-react';
 import type { FC, MouseEventHandler } from 'react';
 import React from 'react';
-import { Button } from './button';
+import { Button } from './Button';
 import { WalletIcon } from './wallet-icon';
 
 export interface WalletListItemProps {
