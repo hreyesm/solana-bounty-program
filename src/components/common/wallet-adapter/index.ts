3853a9f1d497d320b04444a77f0c4dd671cb1e8c
William Wang <swang45@uw.edu>
2022-08-11 09:30:23 -0700

    minor

--- src/components/common/wallet-adapter/index.ts
+++ src/components/common/wallet-adapter/index.ts
@@ -3,6 +3,5 @@ export * from './wallet-connect-button';
 export * from './wallet-modal';
 export * from './wallet-modal-button';
 export * from './wallet-modal-provider';
-export * from './WalletDisconnectButton';
 export * from './wallet-icon';
 export * from './wallet-multi-button';
